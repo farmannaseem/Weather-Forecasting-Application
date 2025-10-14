fc2d532a04ec2587aa3d2436c7b73c381f1262de
farmannaseem <farmannaseem789@gmail.com>
2025-10-14 12:36:52 +0530

    Updates

--- assets/vite.config-BUbTAhlm.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
-
-export default defineConfig({
-  base: '/Weather-Forecasting-Application/',  // ✔️ correct spelling!
-  plugins: [preact()],
-});
